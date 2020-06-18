a4b1131f65330faf8b58bb9858e0a7530fdd411d
anahnas <tomic.ana36@gmail.com>
2020-06-19 00:04:40 +0200

    finished form for adding new advertisement

--- src/app/new-ad/new-ad.component.ts
+++ src/app/new-ad/new-ad.component.ts
@@ -6,6 +6,12 @@ import {CarBrand} from '../model/carBrand';
 import {CarModel} from '../model/carModel';
 import {Car} from '../model/car';
 import {CarClass} from '../model/carClass';
+import {FuelType} from '../model/fuelType';
+import {Transmission} from '../model/transmission';
+import {faCalendar} from '@fortawesome/free-solid-svg-icons';
+import * as moment from 'moment';
+import {DatePipe} from '@angular/common';
+import {NgbDatepickerConfig, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-new-ad',
@@ -21,10 +27,17 @@ export class NewAdComponent implements OnInit {
   carModels: CarModel[] = [];
   carBrands: CarBrand[] = [];
   carClasses: CarClass[] = [];
-
-
+  fuelTypes: FuelType[] = [];
+  transmissions: Transmission[] = [];
+  faCalendar = faCalendar;
 
   constructor(private newAdService: NewAdService, private router: Router) {
+    this.advertisement.car = new Car();
+    this.advertisement.car.carBrand = new CarBrand();
+    this.advertisement.car.carModel = new CarModel();
+    this.advertisement.car.waiver = false;
+    this.advertisement.car.limitedKms = false;
+
   }
 
 
@@ -39,13 +52,24 @@ export class NewAdComponent implements OnInit {
     this.newAdService.getCarClass().subscribe(data3 => {
       this.carClasses = data3;
     });
-
+    this.newAdService.getFuelType().subscribe(data4 => {
+    this.fuelTypes = data4;
+    });
+    this.newAdService.getTransmission().subscribe(data5 => {
+      this.transmissions = data5;
+    });
 
   }
 
   onSubmit() {
     console.log(this.advertisement);
+    this.newAdService.createAdvertisement(this.advertisement).subscribe(result => {
+      alert('Success!');
+      this.router.navigate(['/newAd']);
 
+    });
 
   }
+
+
 }
