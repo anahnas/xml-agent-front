d5e136aabecc02d06deb8d12507436d61db06c4a
Jelena Garić <jeca_997@hotmail.com>
2020-06-18 19:38:57 +0200

    Added editing car and posting a comment.

--- src/app/car.service.ts
+++ src/app/car.service.ts
@@ -4,26 +4,68 @@ import { Observable } from 'rxjs';
 import {  catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { Car } from './model/car';
+import {CarBrand} from './model/carBrand';
+import {CarModel} from "./model/carModel";
+import {CarClass} from "./model/carClass";
+import {TransmissionType} from "./model/transmissionType";
+import {FuelType} from "./model/fuelType";
+import {Rating} from "./model/rating";
 
 @Injectable()
 export class CarService {
 
-    private _getAdUrl = 'http://localhost:8086/car';
+    private _adUrl = 'http://localhost:8086/car';
+  private _getCarBrandsUrl = 'http://localhost:8086/carBrand';
+  private _getCarModelsUrl = 'http://localhost:8086/carModel';
+  private _getCarClassesUrl = 'http://localhost:8086/carClass';
+  private _getTransmissionUrl = 'http://localhost:8086/transmission';
+  private _getFuelTypesUrl = 'http://localhost:8086/fuelType';
 
     constructor(private _http: HttpClient) { }
 
     getAds(): Observable<Car[]> {
-        return this._http.get<Car[]>(this._getAdUrl).pipe(
+        return this._http.get<Car[]>(this._adUrl).pipe(
                     catchError(this.handleError));
     }
 
+    getCarBrands(): Observable<CarBrand[]> {
+      return this._http.get<CarBrand[]>(this._getCarBrandsUrl).pipe(
+        catchError(this.handleError));
+    }
+
+    getCarModels(): Observable<CarModel[]> {
+      return this._http.get<CarModel[]>(this._getCarModelsUrl).pipe(
+        catchError(this.handleError));
+    }
+
+    getCarClasses(): Observable<CarClass[]> {
+      return this._http.get<CarClass[]>(this._getCarClassesUrl).pipe(
+        catchError(this.handleError));
+    }
+
+    getTransmissionTypes(): Observable<TransmissionType[]> {
+      return this._http.get<TransmissionType[]>(this._getTransmissionUrl).pipe(
+        catchError(this.handleError));
+    }
+
+    getFuelTypes(): Observable<FuelType[]> {
+      return this._http.get<FuelType[]>(this._getFuelTypesUrl).pipe(
+        catchError(this.handleError));
+    }
+
     getAd(carId: string): Observable<Car> {
-        return this._http.get<Car>(this._getAdUrl + '/' + carId).pipe(
+        return this._http.get<Car>(this._adUrl + '/' + carId).pipe(
                     catchError(this.handleError));
+    }
+
+    updateCar(car :Car): Observable<Car> {
+      return this._http.post<Car>(this._adUrl, car).pipe(
+        catchError(this.handleError));
     }
 
     private handleError(err: HttpErrorResponse) {
         console.log(err.message);
         return throwError(err.message);
     }
+
 }
--- src/app/model/rating.ts
+++ src/app/model/rating.ts
@@ -1,8 +1,10 @@
 import { User } from './user';
+import {Car} from "./car";
 
 export class Rating {
-    id:String;
-    comment:String;
-    userDTO:User;
+    id: String;
+    comment: String;
+    userDTO: User = new User();
+    carDTO: Car = new Car();
+    rating: number;
   }
-  
